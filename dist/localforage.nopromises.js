be121ae6184c10d1f55edcc50ddab8210d813e3a
Thodoris Greasidis <thgreasi@gmail.com>
2017-08-14 14:44:16 +0300

    chore: add dist for memory storage driver

--- dist/localforage.nopromises.js
+++ dist/localforage.nopromises.js
@@ -1717,17 +1717,242 @@ var localStorageWrapper = {
     keys: keys$2
 };
 
+var storageRepository = {};
+
+// Config the backend, using options set in the config.
+function _initStorage$3(options) {
+    var self = this;
+
+    var dbInfo = {};
+    if (options) {
+        for (var i in options) {
+            dbInfo[i] = options[i];
+        }
+    }
+
+    var database = storageRepository[dbInfo.name] = storageRepository[dbInfo.name] || {};
+    var table = database[dbInfo.storeName] = database[dbInfo.storeName] || {};
+    dbInfo.db = table;
+
+    self._dbInfo = dbInfo;
+    dbInfo.serializer = localforageSerializer;
+
+    return Promise$1.resolve();
+}
+
+// Remove all keys from the datastore, effectively destroying all data in
+// the app's key/value store!
+function clear$3(callback) {
+    var self = this;
+    var promise = self.ready().then(function () {
+        var db = self._dbInfo.db;
+
+        for (var key in db) {
+            if (db.hasOwnProperty(key)) {
+                delete db[key];
+            }
+        }
+    });
+
+    executeCallback(promise, callback);
+    return promise;
+}
+
+// Retrieve an item from the store. Unlike the original async_storage
+// library in Gaia, we don't modify return values at all. If a key's value
+// is `undefined`, we pass that value to the callback function.
+function getItem$3(key, callback) {
+    var self = this;
+
+    key = normalizeKey(key);
+
+    var promise = self.ready().then(function () {
+        var db = self._dbInfo.db;
+        var result = db[key];
+
+        if (result === undefined) {
+            return null;
+        }
+
+        if (result) {
+            result = self._dbInfo.serializer.deserialize(result);
+        }
+
+        return result;
+    });
+
+    executeCallback(promise, callback);
+    return promise;
+}
+
+// Iterate over all items in the store.
+function iterate$3(iterator, callback) {
+    var self = this;
+
+    var promise = self.ready().then(function () {
+        var db = self._dbInfo.db;
+
+        var iterationNumber = 1;
+        for (var key in db) {
+            if (db.hasOwnProperty(key)) {
+                var value = db[key];
+
+                if (value) {
+                    value = self._dbInfo.serializer.deserialize(value);
+                }
+
+                value = iterator(value, key, iterationNumber++);
+
+                if (value !== void 0) {
+                    return value;
+                }
+            }
+        }
+    });
+
+    executeCallback(promise, callback);
+    return promise;
+}
+
+// Same as localStorage's key() method, except takes a callback.
+function key$3(n, callback) {
+    var self = this;
+    var promise = self.ready().then(function () {
+        var db = self._dbInfo.db;
+        var result = null;
+        var index = 0;
+
+        for (var key in db) {
+            if (db.hasOwnProperty(key)) {
+                if (n === index) {
+                    result = key;
+                    break;
+                }
+                index++;
+            }
+        }
+
+        return result;
+    });
+
+    executeCallback(promise, callback);
+    return promise;
+}
+
+function keys$3(callback) {
+    var self = this;
+    var promise = self.ready().then(function () {
+        var db = self._dbInfo.db;
+        var keys = [];
+
+        for (var key in db) {
+            if (db.hasOwnProperty(key)) {
+                keys.push(key);
+            }
+        }
+
+        return keys;
+    });
+
+    executeCallback(promise, callback);
+    return promise;
+}
+
+// Supply the number of keys in the datastore to the callback function.
+function length$3(callback) {
+    var self = this;
+    var promise = self.keys().then(function (keys) {
+        return keys.length;
+    });
+
+    executeCallback(promise, callback);
+    return promise;
+}
+
+// Remove an item from the store, nice and simple.
+function removeItem$3(key, callback) {
+    var self = this;
+
+    key = normalizeKey(key);
+
+    var promise = self.ready().then(function () {
+        var db = self._dbInfo.db;
+        if (db.hasOwnProperty(key)) {
+            delete db[key];
+        }
+    });
+
+    executeCallback(promise, callback);
+    return promise;
+}
+
+// Set a key's value and run an optional callback once the value is set.
+// Unlike Gaia's implementation, the callback function is passed the value,
+// in case you want to operate on that value only after you're sure it
+// saved, or something like that.
+function setItem$3(key, value, callback) {
+    var self = this;
+
+    key = normalizeKey(key);
+
+    var promise = self.ready().then(function () {
+        // Convert undefined values to null.
+        // https://github.com/mozilla/localForage/pull/42
+        if (value === undefined) {
+            value = null;
+        }
+
+        // Save the original value to pass to the callback.
+        var originalValue = value;
+
+        function serializeAsync(value) {
+            return new Promise$1(function (resolve, reject) {
+                self._dbInfo.serializer.serialize(value, function (value, error) {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(value);
+                    }
+                });
+            });
+        }
+
+        return serializeAsync(value).then(function (value) {
+            var db = self._dbInfo.db;
+            db[key] = value;
+            return originalValue;
+        });
+    });
+
+    executeCallback(promise, callback);
+    return promise;
+}
+
+var memoryStorage = {
+    _driver: 'memoryStorage',
+    _initStorage: _initStorage$3,
+    iterate: iterate$3,
+    getItem: getItem$3,
+    setItem: setItem$3,
+    removeItem: removeItem$3,
+    clear: clear$3,
+    length: length$3,
+    key: key$3,
+    keys: keys$3
+};
+
 // Custom drivers are stored here when `defineDriver()` is called.
 // They are shared across all instances of localForage.
 var CustomDrivers = {};
 
 var DriverType = {
     INDEXEDDB: 'asyncStorage',
+    WEBSQL: 'webSQLStorage',
     LOCALSTORAGE: 'localStorageWrapper',
-    WEBSQL: 'webSQLStorage'
+    MEMORY: 'memoryStorage'
 };
 
-var DefaultDriverOrder = [DriverType.INDEXEDDB, DriverType.WEBSQL, DriverType.LOCALSTORAGE];
+var DefaultDriverOrder = [DriverType.INDEXEDDB, DriverType.WEBSQL, DriverType.LOCALSTORAGE, DriverType.MEMORY];
 
 var LibraryMethods = ['clear', 'getItem', 'iterate', 'key', 'keys', 'length', 'removeItem', 'setItem'];
 
@@ -1753,6 +1978,8 @@ driverSupport[DriverType.INDEXEDDB] = isIndexedDBValid();
 driverSupport[DriverType.WEBSQL] = isWebSQLValid();
 
 driverSupport[DriverType.LOCALSTORAGE] = isLocalStorageValid();
+
+driverSupport[DriverType.MEMORY] = true;
 
 var isArray = Array.isArray || function (arg) {
     return Object.prototype.toString.call(arg) === '[object Array]';
@@ -1803,6 +2030,7 @@ var LocalForage = function () {
 
         this.INDEXEDDB = DriverType.INDEXEDDB;
         this.LOCALSTORAGE = DriverType.LOCALSTORAGE;
+        this.MEMORY = DriverType.MEMORY;
         this.WEBSQL = DriverType.WEBSQL;
 
         this._defaultConfig = extend({}, DefaultConfig);
@@ -1927,6 +2155,8 @@ var LocalForage = function () {
                         return asyncStorage;
                     case self.LOCALSTORAGE:
                         return localStorageWrapper;
+                    case self.MEMORY:
+                        return memoryStorage;
                     case self.WEBSQL:
                         return webSQLStorage;
                 }
